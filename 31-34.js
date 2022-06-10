8dbc75e1e2d6104dafd215847f733cb61e826fd8
Muhammad-Hunain <65266923+Muhammad-Hunain@users.noreply.github.com>
2022-06-10 13:59:54 +0500

    Completed

---
+++ 31-34.js
@@ -0,0 +1,123 @@
+
+
+
+//======================================== Question Number :01==================================
+// var right=new Date()
+// console.log(right)
+
+//======================================== Question Number :02==================================
+
+// var aja=right.getMonth();
+// console.log(aja)
+// var array=["jan",'Feb',"march","april","may",'june','july','august','september','october','november','december']
+// for(var i=0;i<array.length;i++){
+//     if(i==aja){
+//         console.log("current month" ,array[i])
+//         break
+        
+//     }
+// }
+
+
+//======================================== Question Number :03==================================
+
+// var day=['Mon',"tue","wed","thu","fri","sat","sun"]
+// var day1=right.getDay()
+// console.log(day1)
+// for(var i=0;i<day.length;i++){
+//     if(i==day1){
+//         console.log("Today is" ,day[i-1])
+//         break
+        
+//     }
+// }
+
+
+//======================================== Question Number :04==================================
+// var i=right.getFullYear()
+// var h=Date.now()-right.getTime()
+// console.log(h)
+// var k=new Date(h)
+// console.log(k.getFullYear())
+// var year=k.getUTCFullYear()
+// var age=Math.abs(i-k)
+// console.log(age,year)
+
+
+// var day=['Mon',"tue","wed","thu","fri","sat","sun"]
+// var day1=right.getDay()
+// console.log(day1)
+// for(var i=0;i<day.length;i++){
+//        if(i==day1){
+//         console.log("It's fun day")
+//         break
+        
+//     }
+// }
+
+//======================================== Question Number :05==================================
+
+// var date=right.getDate()
+// if(date<16){
+//     console.log("first fifteen days of the month")
+// }else{
+//     console.log("lasr fifteen days of the month")
+// }
+
+//======================================== Question Number :06==================================
+
+// var j=Date.now()
+// console.log("Elapsed milisecond since jan 1 1970 ",j,"Sec")
+// var f=j/60000;
+// console.log("Elapsed minute since jan 1 1970", f,"Min")
+
+//======================================== Question Number :07==================================
+
+// var j=right.getHours();
+// console.log(j)
+// if(j<12){
+//     console.log("AM")
+// }
+// else{
+//     console.log("PM")
+// }
+//======================================== Question Number :14==================================
+// var dDiff = msDiff / (1000 * 60 * 60 * 24)
+
+// var j=Date.now("2015")
+// var y=j/1000
+// var f=right.getSeconds()
+// var t=Math.abs(f-y);
+// console.log(t)
+
+// var j=right.getHours();
+// var s=right.getSeconds();
+// var m=right.getMinutes();
+
+// console.log(right)
+// console.log("1 Hour age it was",j-1,":",m,":",s)
+
+// // new question start from here:
+
+// var name="Muhammad usman";// here declare name variable
+// var a=prompt("Enter number of units you used")// to get value from user
+// var b=16    
+// var charges=a*16;
+// var total=charges;
+// var late=1000;
+// var after=total+1000;
+// console.log("Customer Name:",name)
+// var month=right.getMonth();
+// var array=["jan",'Feb',"march","april","may",'june','july','august','september','october','november','december']
+// for(var i=0;i<array.length;i++){
+//     if(i==month){
+//         console.log("Month: " ,array[i])
+//         break
+        
+//     }
+// }
+// console.log("Number of Units:",a)
+// console.log("Charges per unit",b);
+// console.log("Net amount payable within due date: ",total);
+// console.log("Late amount charges is: ",late)
+// console.log("Net amount payable after due date: ",after);
---
+++ 35-38.js
@@ -0,0 +1,188 @@
+//======================================== Question Number :01==================================
+// function date(){
+//     var h=new Date();
+//     return h;
+// }
+// var a;
+// console.log(date(a))
+//======================================== Question Number :02==================================
+
+// function fullname(a,b){
+// return a+" "+b;
+// }
+
+// var a=prompt("Enter first name")
+// var b=prompt("Enter last name")
+// console.log(fullname(a,b))
+
+//======================================== Question Number :03==================================
+
+// function Add_number(a,b){
+//     return a+b;
+//     }
+//     var a=parseInt( prompt("Enter first name"))
+//     var b=+prompt("Enter last name")
+//     console.log(Add_number(a,b))
+
+//======================================== Question Number :04==================================
+
+// function calculator(a,b,c){
+//     var h=prompt("enter operator");
+//     switch(h){
+//         case '+':
+//             return a+b;
+//             break
+//         case '-':
+//             return a-b
+//             break;
+//         case '*':
+//             return a*b;
+//             break
+//             case '/':
+//             return a/b;
+//             break;
+//         default:
+//             return "invalid input"
+//     }
+// }
+// var a=+prompt("enter 1st number")
+// var b=+prompt("enter 2nd number")
+
+// console.log(calculator(a,b))
+//======================================== Question Number :05==================================
+// var a=+prompt("entet number");
+// function square(a){
+//     return a*a;
+// }
+//======================================== Question Number :06==================================
+
+// var a=+prompt("entet number");
+
+// function factorial(a){
+//     var c=1;
+//     for(var i=a;i>=1;i--){
+//         c*=i
+//     }
+// return c;
+// }
+// var a=+prompt("enter the number")
+// console.log(factorial(a))
+
+//======================================== Question Number :07==================================
+
+// function display(a,b){
+    
+// var a=+prompt("enter starting number")
+// var b=+prompt("enter ending number")
+//     for(var i=a;i<=b;i++){
+//         console.log(i)
+//     }
+    
+
+// }
+// display()
+//======================================== Question Number :08==================================
+
+// function calculatehyp(){
+//     var res=a**2+b**2;
+//     var c=square(res);
+//     return c;
+
+// }
+// function square(d){
+// return Math.sqrt(d)
+    
+// }
+// var a=+prompt("enter 1st number")
+// var b=+prompt("enter 2nd number")
+// console.log("the hypotenus is:"+calculatehyp(a,b))
+
+
+
+// function added(a,b){
+//     var a=prompt ("enter first name")
+//     var b=prompt ("enter last name")
+//     var c=a+" "+b;
+//     return c;
+// }
+// var a,b;
+// console.log(added(a,b))
+
+// function added(a,b){
+//     var a=+prompt ("enter first nubmer")
+//     var b=+prompt ("enter last nubmer")
+//     var c=a+b;
+//     return c;
+// }
+// var a,b;
+// console.log(added(a,b))
+
+// function square(a){
+//     var a=+prompt("enter number")
+//     var b=a**2;
+//     return b;
+// }
+// var a;
+// console.log(square(a))
+//======================================== Question Number :09==================================
+
+
+// function area(a,b){
+//     var a=+prompt("enter base")
+//     var b=+prompt("enter height")
+//     var c=a*b
+//     return c;
+// }
+// var a,b;
+// console.log(area(a,b))
+
+// function area1(a,b){
+//     // var a=+prompt("enter base")
+//     // var b=+prompt("enter height")
+//     var c=a*b
+//     return c;
+// }
+// var a,b;
+// console.log(area1(2,3))
+//======================================== Question Number :14==================================
+
+
+// function calccircumference(r){
+//     const PI=3.14;
+//     var r=+prompt("enter radius ")
+//     var h=2*PI*r;
+//     return "the circumferenve of circle is: "+h
+// }
+// function calcarea(){
+//     var r=+prompt("Enter radius")
+//     const Pi=3.14;
+//     var a=Pi*r**2;
+//     return a;
+// }
+// var a,b;
+// console.log(calccircumference(a))
+// // console.log(calcarea(b))
+
+// var a=+prompt("enter 2dt number")
+// var b=+prompt("enter 3nd number")
+// var c=+prompt("enter 3rd number")
+
+// function area(s){
+//      var t=para();
+//     var t=t*(t-a)*(t-b)*(t-c)
+
+// return t; 
+// }
+
+// function para(s)
+// {
+//     var s=(a+b+c)/2;
+//     return s;
+// }
+
+// var g
+// // console.log(para(g))
+// var c
+// console.log(area(c))
+
+// =========================================
